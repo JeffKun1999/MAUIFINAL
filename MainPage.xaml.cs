a8698ca4c8e3f629c64320d557eedb799f35060e
JeffKun1999 <jefferson.ruiz.pico@udla.edu.ec>
2024-05-27 13:39:46 -0500

    Commit del .cs y .gitignore

---
+++ MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿
+namespace MJSCJR_TacoslaEnredadaInterfaz
+{
+    public partial class MainPage : ContentPage
+    {
+    
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnBtnComprar1Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SecondPage());
+            
+
+        }
+    }
+
+}
